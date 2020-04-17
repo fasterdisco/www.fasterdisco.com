96a796cd590c42db629ae1e092652dbf64486a23
Eric Redon <eric.redon@coox.org>
2020-04-17 10:48:03 +0200

    Add Prettier with ESLint integration

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,13 +15,21 @@ module.exports = {
     sourceType: 'module',
   },
 
-  // https://eslint.org/docs/user-guide/configuring#using-eslint-recommended
-  extends: 'eslint:recommended',
+  // https://eslint.org/docs/user-guide/configuring#extending-configuration-files
+  extends: [
+    // Enforce rules that report common problems
+    // https://eslint.org/docs/user-guide/configuring#using-eslint-recommended
+    'eslint:recommended',
+    // Enforce both eslint-plugin-prettier and eslint-config-prettier
+    // https://github.com/prettier/eslint-plugin-prettier#recommended-configuration
+    'plugin:prettier/recommended',
+  ],
 
   overrides: [
     {
       files: [
         '.eslintrc.js',
+        'prettier.config.js',
         'webpack.config.js',
       ],
       env: {
