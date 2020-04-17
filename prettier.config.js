50b79b54785ab4c6693dcea0af1de28840f220d2
Eric Redon <eric.redon@coox.org>
2020-04-17 11:04:44 +0200

    Adjust Prettier settings for CSS + HTML

--- prettier.config.js
+++ prettier.config.js
@@ -4,4 +4,14 @@ module.exports = {
   // Enforce usage of single quotes for string literals
   // https://prettier.io/docs/en/options.html#quotes
   singleQuote: true,
+
+  // https://prettier.io/docs/en/configuration.html#configuration-overrides
+  overrides: [
+    {
+      files: ['*.css', '*.html'],
+      options: {
+        printWidth: 120,
+      },
+    },
+  ],
 };
