5074ea71b7005ad8fe3f351a27ee9c63f6c038e8
Éric Redon <eric.redon@coox.org>
2022-08-28 00:43:00 +0200

    Rebuild with Astro

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,28 @@
 module.exports = {
-  // https://eslint.org/docs/user-guide/configuring#specifying-environments
-  env: {
-    // Enable support of all ECMAScript 2020 globals
-    // and automatically sets `parserOption.ecmaVersion` to 11
-    // to specify usage of ECMAScript syntax version 2020
-    es2020: true,
-    // Enable support of Node.js global variables and Node.js scoping
-    node: true,
-  },
+    // https://eslint.org/docs/latest/user-guide/configuring/language-options#specifying-environments
+    env: {
+        // Enable support of all ECMAScript 2022 globals
+        // and automatically sets `parserOption.ecmaVersion` to 13
+        // to specify usage of ECMAScript syntax version 2022
+        es2020: true,
 
-  // https://eslint.org/docs/user-guide/configuring#specifying-parser-options
-  parserOptions: {
-    // Specify that code is in ECMAScript modules
-    sourceType: 'module',
-  },
-
-  // https://eslint.org/docs/user-guide/configuring#extending-configuration-files
-  extends: [
-    // Enforce rules that report common problems
-    // https://eslint.org/docs/user-guide/configuring#using-eslint-recommended
-    'eslint:recommended',
-    // Enforce both eslint-plugin-prettier and eslint-config-prettier
-    // https://github.com/prettier/eslint-plugin-prettier#recommended-configuration
-    'plugin:prettier/recommended',
-  ],
-
-  overrides: [
-    {
-      files: ['src/**/*.js'],
-      env: {
-        // Enable support of browser global variables
-        browser: true,
-        // Disable support of Node.js global variables and Node.js scoping
-        node: false,
-      },
+        // Enable support for Node.js global variables and Node.js scoping.
+        node: true,
     },
-  ],
+
+    // https://eslint.org/docs/latest/user-guide/configuring/language-options#specifying-parser-options
+    parserOptions: {
+        // Specify that code is in ECMAScript modules
+        sourceType: 'module',
+    },
+
+    // https://eslint.org/docs/latest/user-guide/configuring/configuration-files#extending-configuration-files
+    extends: [
+        // Enforce rules that report common problems
+        // https://ota-meshi.github.io/eslint-plugin-astro/user-guide/#configuration
+        'plugin:astro/recommended',
+        // Enforce both eslint-plugin-prettier and eslint-config-prettier
+        // https://github.com/prettier/eslint-plugin-prettier#recommended-configuration
+        'plugin:prettier/recommended',
+    ],
 };
